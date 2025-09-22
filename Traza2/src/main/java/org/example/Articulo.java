e1ad8d6a8840ef72bceb7931574afc4dd8ca1f92
Matias3096 <archivosfacultad09@gmail.com>
2025-09-22 08:53:44 -0300

    Terminamos en un ratooo

--- Traza2/src/main/java/org/example/Articulo.java
+++ Traza2/src/main/java/org/example/Articulo.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+
 public class Articulo {
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String nombre;
+    private Double precio;
+    private Categoria categoria;
+    private UnidadMedida unidadMedida;
+    private Boolean esParaElaborar;  // Si este insumo se usa para elaborar otros, sirve para marcar insumos que participan en fabricacion, respetando el enunciado el tp
+    private TipoArticulo tipoArticulo;  //Insumo Manufacturado
+
+    @ToString.Exclude
+    @Builder.Default
+
+    private Set<Imagen> imagenes = new HashSet<>();
+
+    @ToString.Exclude
+    @Builder.Default
+    private Set<DetalleArticuloManufacturado> detalleArticulos = new HashSet<>();
+
+
+    //Elegi un solo tipo de Articulo con tipo para evitar la jerarquia de clases, es decir menos complejidad para el tp y las busquedas
+    //Imagenes y detalles estan listos para manufacturados, para insumos permanecen vacios
+
 }
